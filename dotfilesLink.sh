16ec7b4d07cb1d4795fe48529e9ec4977a91e7fa
barleytea <barleytea362@gmail.com>
2018-02-11 05:30:45 +0900

    edited link setting

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/dotfiles/.vim ~/.vim
