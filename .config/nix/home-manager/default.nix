ebd7b0b7b42a71bdf5b49793c6f6c754a0d4c687
barleytea <barleytea362@gmail.com>
2024-10-01 16:24:08 +0900

    :wrench: Brew to nix

--- .config/nix/home-manager/default.nix
+++ .config/nix/home-manager/default.nix
@@ -17,8 +17,48 @@ in {
     homeDirectory = "/Users/${username}";
     stateVersion = "24.05";
     packages = with pkgs; [
-      git
+      arp-scan
+      asdf
       curl
+      gat
+      bottom
+      glib
+      cairo
+      eza
+      fd
+      fftw
+      fish
+      fontforge
+      fzf
+      gh
+      ghq
+      git
+      gitflow
+      go
+      gnugrep
+      stack
+      hub
+      jpegoptim
+      jq
+      krb5
+      kubectx
+      mas
+      maven
+      mecab
+      minikube
+      neofetch
+      peco
+      postgresql
+      protobuf
+      pyenv
+      rbenv
+      ripgrep
+      socat
+      starship
+      tmux
+      tree
+      zplug
+      zsh
     ];
   };
 
