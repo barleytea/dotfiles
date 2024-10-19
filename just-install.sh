ee42eba72a5e0701d3ec32dc8f335760524fd6d7
barleytea <barleytea362@gmail.com>
2024-10-19 12:42:27 +0900

    test

--- just-install.sh
+++ just-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p ~/.local/bin
-curl --retry 5 --retry-delay 5 --proto '==https' --tlsv1.2 -H 'User-Agent: Chrome' -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
+curl -v --connect-timeout 10 --max-time 30 --retry 5 --retry-delay 5 --proto '==https' --tlsv1.2 -H 'User-Agent: Chrome' -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
