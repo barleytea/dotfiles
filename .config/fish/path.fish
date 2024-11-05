4086f2e6f077d23c2bbf55b40c219c8710e0d2f4
barleytea <barleytea362@gmail.com>
2024-11-05 18:18:27 +0900

    test

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -10,5 +10,6 @@ set -Ux NIX_PATH $HOME/.nix-defexpr/channels /nix/var/nix/profiles/per-user/root
 if test -e $HOME/.nix-profile/etc/profile.d/nix.sh
   source $HOME/.nix-profile/etc/profile.d/nix.sh
 end
+set -Ux NIX_CONF_DIR $HOME/.config/nix
 
 # fish_add_path $HOME/.local/bin 
