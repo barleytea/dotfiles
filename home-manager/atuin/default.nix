03e8af42d57849f423b34dee0a582811135a9137
barleytea <barleytea362@gmail.com>
2025-01-08 19:05:57 +0900

    update atuin settings

--- home-manager/atuin/default.nix
+++ home-manager/atuin/default.nix
@@ -3,5 +3,16 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+
+    flags = [ "--disable-up-arrow" ];
+
+    settings = {
+      auto_sync = false;
+      style = "compact";
+      enter_accept = false;
+      keymap_mode = "vim-normal";
+      search_mode = "fuzzy";
+      filter_mode = "host"; # global, host, session, directory
+    };
   };
 }
