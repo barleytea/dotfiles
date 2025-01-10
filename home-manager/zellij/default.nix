e5812908d081a5318aee0b469f1d53b5cbb8c5d8
barleytea <barleytea362@gmail.com>
2025-01-10 16:36:14 +0900

    fix: :bug: fix zellij copy on select

--- home-manager/zellij/default.nix
+++ home-manager/zellij/default.nix
@@ -1,9 +1,24 @@
+{ pkgs, ... }:
 let
   readFile = builtins.readFile;
+  configTemplate = builtins.readFile ./config.kdl;
+  
+  copyCommand = if pkgs.stdenv.isDarwin then
+    "pbcopy"
+  else if pkgs.stdenv.isLinux then
+    "xsel --clipboard --input"
+  else
+    "pbcopy";
+    
+  # テンプレート内の copy_command を置換
+  config = builtins.replaceStrings 
+    ["copy_command \"pbcopy\""]
+    ["copy_command \"${copyCommand}\""]
+    configTemplate;
 in {
   programs.zellij = {
     enable = true;
   };
 
-  home.file.".config/zellij/config.kdl".text = readFile ./config.kdl;
+  home.file.".config/zellij/config.kdl".text = config;
 }
