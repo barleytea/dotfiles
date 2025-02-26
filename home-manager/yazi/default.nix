c37e18e06f681610808cb0c432eb4537d0e54957
barleytea <barleytea362@gmail.com>
2025-02-27 00:46:55 +0900

    ci: :ferris_wheel: fix build error

---
+++ home-manager/yazi/default.nix
@@ -0,0 +1,9 @@
+{
+  programs.yazi = {
+    enable = true;
+  };
+
+  # xdg.configFile."yazi/yazi.toml".source = ./yazi.toml;
+  # xdg.configFile."yazi/theme.toml".source = ./theme.toml;
+  # xdg.configFile."yazi/keymap.toml".source = ./keymap.toml;
+}
