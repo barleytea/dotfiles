c68b130ebbd5369d3ba2638eec11e445fd4d4237
barleytea <barleytea362@gmail.com>
2025-04-02 11:16:06 +0900

    fix: :bug: specify golongci-lint version to v1.64.5

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -4,6 +4,33 @@
   ...
 }: let
   utils = import ./utils/utils.nix { inherit pkgs; };
+  # golangci-lintの特定バージョンを取得するための関数
+  getGolangciLint = pkgs: let
+    version = "1.64.5";
+    platform = if pkgs.stdenv.hostPlatform.isDarwin then
+      if pkgs.stdenv.hostPlatform.isAarch64 then "darwin-arm64"
+      else "darwin-amd64"
+    else pkgs.stdenv.hostPlatform.system;
+  in pkgs.stdenv.mkDerivation {
+    name = "golangci-lint-${version}";
+    inherit version;
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/golangci/golangci-lint/releases/download/v${version}/golangci-lint-${version}-${platform}.tar.gz";
+      sha256 = if pkgs.stdenv.hostPlatform.isDarwin && pkgs.stdenv.hostPlatform.isAarch64 then
+        "sha256-jE8R7zoi1hDdWDagnJjpRLQFYk+TLyDH5yrnirxVIxE=" # darwin-arm64用ハッシュ
+      else
+        "sha256-doHD6RlJEDBVjvObbMr0m+Gz0Z3mEdMMAq7IKNrYIsE="; # それ以外のハッシュ
+    };
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r golangci-lint-${version}-${platform}/golangci-lint $out/bin/
+      chmod +x $out/bin/golangci-lint
+    '';
+  };
 in {
 
   nixpkgs = {
@@ -61,7 +88,7 @@ in {
       gitflow
       glib
       gnugrep
-      golangci-lint
+      (getGolangciLint pkgs)  # 自前でバージョン固定したパッケージを使う
       google-chrome
       grpcurl
       hub
