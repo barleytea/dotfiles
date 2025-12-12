ad4781c47b6f8c470f186b34bd45aacd76369f41
barleytea <barleytea362@gmail.com>
2025-12-12 16:37:01 +0900

    feat: :sparkles: fix version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Darwin";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
