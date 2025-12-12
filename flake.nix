ab13e70775e458c844683f94133edd690cbe0a4e
barleytea <barleytea362@gmail.com>
2025-12-12 18:00:50 +0900

    Revert "fix: :bug: fix version"

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
